69696636566846aed6e67a4688bc3a9e888b8271
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2021-05-31 17:02:20 +0530

    Sound Function
    Some Basic Function for sound management.

--- Snake_Game.py
+++ Snake_Game.py
@@ -18,3 +18,19 @@ from email import encoders  # To encode file into msg formate.
 import gspread #For google sheet
 from oauth2client.service_account import ServiceAccountCredentials
 from oauth2client import crypt
+
+pygame.init()
+
+def load_sound(sound_file):
+    return pygame.mixer.Sound(sound_file)
+
+def play_sound(sound_file,loop = 0,maxmim_time = None):
+    if maxmim_time == None:
+        pygame.mixer.Sound.play(sound_file, loop)
+    else:
+        pygame.mixer.Sound.play(sound_file, loop, maxmim_time)
+
+def set_valuem(sound_file, valuem):
+    #Max Value 1.0
+    #Min Value 0.0
+    pygame.mixer.Sound.set_volume(sound_file, valuem)
